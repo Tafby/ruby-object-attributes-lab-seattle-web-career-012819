d9d386603c750eeb85ae6053e1444be690a167fe
Tiffany Carter <tafby88@gmail.com>
2019-01-29 19:24:56 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,10 +5,8 @@ class Person
     @last_name = last_name
   end
  
-  def name=(full_name)
-    first_name, last_name = full_name.split
-    @first_name = first_name
-    @last_name = last_name
+  def name=(name)
+ @name = name
   end
  
   def name
