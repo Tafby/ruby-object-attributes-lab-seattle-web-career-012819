6e2ac132d43c648b5fe6cdb3131542ebb683467c
Tiffany Carter <tafby88@gmail.com>
2019-01-29 19:23:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,8 @@
 class Dog
- 
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
-  end
- 
+  
   def breed=(breed)
     @breed = breed
+  end
+ 
 
-  end
- 
-  def name
-    "#{@first_name} #{@last_name}".strip
-  end
- 
 end
