73e18654673c995f0593e909ac183f45280ee76f
Tiffany Carter <tafby88@gmail.com>
2019-01-29 19:25:03 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,9 @@
 class Person
  
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
-  end
+  # def initialize(first_name, last_name)
+  #   @first_name = first_name
+  #   @last_name = last_name
+  # end
  
   def name=(name)
  @name = name
